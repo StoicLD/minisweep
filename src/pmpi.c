4fb40bd70f45ec90f229a04df7be33a6d021cd4f
Dong Li <729790146@qq.com>
2022-02-13 16:08:53 -0500

    first attempt

--- src/pmpi.c
+++ src/pmpi.c
@@ -1,8 +1,26 @@
 #include "mpi.h"
 #include "stdio.h"
 
-OMPI_DECLSPEC int MPI_Init(int *argc, char ***argv)
+int MPI_Init(int *argc, char ***argv)
 {
-    printf("use my mpi\n");
-    PMPI_Init(argc, argv);
+    printf("use my mpi init\n");
+    return PMPI_Init(argc, argv);
+}
+
+int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
+                            int tag, MPI_Comm comm)
+{
+    return PMPI_Send(buf, count, datatype, dest, tag, comm);
+}
+
+int MPI_Isend(const void *buf, int count, MPI_Datatype datatype, int dest,
+                             int tag, MPI_Comm comm, MPI_Request *request)
+{
+    return PMPI_Isend(buf, count, datatype, dest, tag, comm, request);
+}
+
+int MPI_Finalize(void)
+{
+    printf("use my mpi finialize\n")
+    return PMPI_Finalize();
 }
